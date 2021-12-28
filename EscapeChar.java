99f0c4ac502532ee1bb835ac422ebf7334e487d7
faishal <faishal2022cse@gmail.com>
2021-12-28 15:26:14 +0530

    Added Escapechar.java

---
+++ EscapeChar.java
@@ -0,0 +1,9 @@
+class EscapeChar
+{
+public static void main(String args[])
+{
+System.out.println("Hello\nhi\twelcome");
+System.out.println("\"Helloworld\"");
+System.out.println("c:\\users");
+}
+}
